1b8c74440f92aebf9707cf9139a71f6a34fb0f6f
Gautam Thapa <gautamthapagg@gmail.com>
2025-08-13 07:58:08 +0545

    add permissions

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
@@ -5,10 +6,35 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("name",)
+        fields = ("username", "email", "name")  # Explicitly list fields
+        field_classes = None  # Ensure no unexpected field classes
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove any unexpected fields
+        if "usable_password" in self.fields:
+            del self.fields["usable_password"]
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "name",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+            "last_login",
+            "date_joined",
+        )
+        field_classes = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove any unexpected fields
+        if "usable_password" in self.fields:
+            del self.fields["usable_password"]
